78eac9a88a147c5ea6492b50167aec29b182e382
dlprk-qa2 <101082466+dlprk-qa2@users.noreply.github.com>
2022-08-16 16:50:05 +0530

    Update s3_logging.tf

--- s3_logging.tf
+++ s3_logging.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+ # required_version = ">= 0.14.9"
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
 resource "aws_s3_bucket" "logging" {
   bucket = "examplebuckettftest"
   acl    = "private"
